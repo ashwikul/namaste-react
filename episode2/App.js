939c5a2c9d1cc67c26e6f2c2c415bf9d0de0f1d8
ashwikul <ashwinikk1990@gmail.com>
2024-04-25 08:39:03 +0530

    episode 2

---
+++ episode2/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading" }, "i am h1 tag"),
+    React.createElement("h2", { id: "heading" }, "i am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "i am h1 tag"),
+    React.createElement("h2", { id: "heading" }, "i am h2 tag"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
